39b767d0130fe94f7f9f0709e4c1cf93fe0de73a
Minhal Muraj <64689808+minhal15@users.noreply.github.com>
2022-12-19 05:41:29 +0000

    extracted top 10 videos and parsed for important data.

--- scraper.py
+++ scraper.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
+import pandas as pd
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 
-YOUTUBE_URL='https://www.youtube.com/feed/trending'
+YOUTUBE_URL='https://www.youtube.com/feed/trending?bp=4gIKGgh0cmFpbGVycw%3D%3D'
 
 def get_driver():
-  chrome_options = Options()
+  chrome_options = Options() 
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--headless')
   chrome_options.add_argument('--disable-dev-shm-usage')
@@ -16,9 +17,31 @@ def get_driver():
 def get_videos(driver):
   VIDEO_DIV_TAG = 'ytd-video-renderer'
   driver.get(YOUTUBE_URL)
-  time.sleep(5)
+  time.sleep(10)
   videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
   return videos
+
+def parse_video(video):
+  title_tag=video.find_element(By.ID, 'video-title')
+  title = title_tag.text
+  url = title_tag.get_attribute('href')
+
+  thumbnail_tag=video.find_element(By.TAG_NAME, 'img')
+  thumbnail_url=thumbnail_tag.get_attribute('src')
+
+  channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
+  channel_name = channel_div.text
+
+  description = video.find_element(By.ID, 'description-text').text
+
+  return {
+    'title': title,
+    'url': url,
+    'thumbnail_url': thumbnail_url,
+    'channel': channel_name,
+    'description': description
+  }
+  
 
 if __name__ == "__main__":
   driver = get_driver()
@@ -26,6 +49,15 @@ if __name__ == "__main__":
   print('Fetching page')
   videos = get_videos(driver)
   
-  print(f'Found {len(videos)}')
+  print(f'Found {len(videos)} no of videos')
 
+  print('Parsing top 10 videos')
+  videos_data = [parse_video(video) for video in videos[:10]]
+
+  print('Save data to CSV')
+  videos_df = pd.DataFrame(videos_data)
+  print(videos_df)
+  videos_df.to_csv('yt_trending.csv')
+  
+  
 
