49aba9495f3d7c983d1c9019f366af33d16fb6db
Minhal Muraj <64689808+minhal15@users.noreply.github.com>
2022-12-19 04:23:46 +0000

    Got the video divs after 2 hours '-_-'

--- scraper.py
+++ scraper.py
@@ -1,7 +1,31 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 
 YOUTUBE_URL='https://www.youtube.com/feed/trending'
 
-response = requests.get(YOUTUBE_URL)
+def get_driver():
+  chrome_options = Options()
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome(options=chrome_options)
+  return driver
 
-print("Status code ", response.status_code)
+def get_videos(driver):
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  driver.get(YOUTUBE_URL)
+  time.sleep(5)
+  videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  return videos
+
+if __name__ == "__main__":
+  driver = get_driver()
+
+  print('Fetching page')
+  videos = get_videos(driver)
+  
+  print(f'Found {len(videos)}')
+
+
